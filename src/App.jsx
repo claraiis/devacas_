28e4e025b99d98f6a21be89ece471da397f836c2
claraiis <claraiglesiasmkt@gmail.com>
2025-10-08 13:00:26 +0200

    update holidays

--- src/App.jsx
+++ src/App.jsx
@@ -23,23 +23,25 @@ const REGIONAL_HOLIDAYS = {
     'VC': [{ date: '2025-10-09', name: 'Día de la Comunidad Valenciana' }]
   },
   '2026': {
-    'AN': [{ date: '2026-02-28', name: 'Día de Andalucía' }],
-    'AR': [{ date: '2026-04-23', name: 'Día de Aragón' }],
-    'AS': [{ date: '2026-09-08', name: 'Día de Asturias' }],
-    'IB': [{ date: '2026-03-01', name: 'Día de las Islas Baleares' }],
-    'CN': [{ date: '2026-05-30', name: 'Día de Canarias' }],
-    'CB': [{ date: '2026-07-28', name: 'Día de Cantabria' }],
-    'CL': [{ date: '2026-04-23', name: 'Día de Castilla y León' }],
-    'CM': [{ date: '2026-05-31', name: 'Día de Castilla-La Mancha' }],
-    'CT': [{ date: '2026-09-11', name: 'Diada de Catalunya' }, { date: '2026-06-24', name: 'Sant Joan' }],
-    'EX': [{ date: '2026-09-08', name: 'Día de Extremadura' }],
-    'GA': [{ date: '2026-07-25', name: 'Día de Galicia' }, { date: '2026-06-24', name: 'San Juan'}, { date: '2026-04-02', name: 'Jueves Santo' }, { date: '2026-03-19', name: 'San José' }],
-    'MD': [{ date: '2026-05-02', name: 'Día de la Comunidad de Madrid' }],
-    'MU': [{ date: '2026-06-09', name: 'Día de la Región de Murcia' }],
-    'NC': [{ date: '2026-09-27', name: 'Día de Navarra' }],
-    'PV': [{ date: '2026-10-25', name: 'Día del País Vasco' }],
-    'RI': [{ date: '2026-06-09', name: 'Día de La Rioja' }],
-    'VC': [{ date: '2026-10-09', name: 'Día de la Comunidad Valenciana' }]
+    'AN': [{ date: '2026-02-28', name: 'Día de Andalucía' }, { date: '2026-04-02', name: 'Jueves Santo' }, { date: '2026-11-02', name: 'Fiesta de Todos los Santos' }, { date: '2026-12-07', name: 'Día de la Constitución' }],
+    'AR': [{ date: '2026-04-23', name: 'Día de Aragón' }, { date: '2026-04-02', name: 'Jueves Santo' }, { date: '2026-11-02', name: 'Fiesta de Todos los Santos' }, { date: '2026-12-07', name: 'Día de la Constitución' }],
+    'AS': [{ date: '2026-09-08', name: 'Día de Asturias' }, { date: '2026-04-02', name: 'Jueves Santo' }, { date: '2026-11-02', name: 'Fiesta de Todos los Santos' }, { date: '2026-12-07', name: 'Día de la Constitución' }],
+    'IB': [{ date: '2026-03-02', name: 'Día de las Islas Baleares' }, { date: '2026-04-02', name: 'Jueves Santo' }, { date: '2026-04-06', name: 'Lunes de Pascua' }, { date: '2026-12-26', name: 'Sant Esteve' }],
+    'CE': [{ date: '2026-09-02', name: 'Día de Ceuta' }, { date: '2026-04-02', name: 'Jueves Santo' },{ date: '2026-08-05', name: 'Nuestra Señora de África' },{ date: '2026-05-27', name: 'Fiesta del Sacrificio - Aid al-Adha' }],
+    'CN': [{ date: '2026-05-30', name: 'Día de Canarias' }, { date: '2026-04-02', name: 'Jueves Santo' }, { date: '2026-11-02', name: 'Fiesta de Todos los Santos' }],
+    'CB': [{ date: '2026-07-28', name: 'Día de las Instituciones de Cantabria' }, { date: '2026-04-02', name: 'Jueves Santo' }, { date: '2026-09-15', name: 'La Bien Aparecida' }, { date: '2026-12-07', name: 'Día de la Constitución' }],
+    'CL': [{ date: '2026-04-23', name: 'Día de Castilla y León' }, { date: '2026-04-02', name: 'Jueves Santo' }, { date: '2026-11-02', name: 'Fiesta de Todos los Santos' }, { date: '2026-12-07', name: 'Día de la Constitución' }],
+    'CM': [{ date: '2026-06-04', name: 'Fiesta del Corpus Christi' }, { date: '2026-04-02', name: 'Jueves Santo' }, { date: '2026-04-06', name: 'Lunes de Pascua' }, { date: '2026-11-02', name: 'Día de Todos los Santos' }],
+    'CT': [{ date: '2026-09-11', name: 'Diada de Catalunya' }, { date: '2026-06-24', name: 'Sant Joan' }, { date: '2026-04-06', name: 'Lunes de Pascua' }, { date: '2026-12-26', name: 'Sant Esteve' }],
+    'EX': [{ date: '2026-09-08', name: 'Día de Extremadura' }, { date: '2026-04-02', name: 'Jueves Santo' }, { date: '2026-11-02', name: 'Fiesta de Todos los Santos' }, { date: '2026-12-07', name: 'Día de la Constitución' }],
+    'GA': [{ date: '2026-07-25', name: 'Santiago Apóstol' }, { date: '2026-06-24', name: 'San Juan'}, { date: '2026-04-02', name: 'Jueves Santo' }, { date: '2026-03-19', name: 'San José' }],
+    'MD': [{ date: '2026-05-02', name: 'Día de la Comunidad de Madrid' }, { date: '2026-04-02', name: 'Jueves Santo' }, { date: '2026-11-02', name: 'Fiesta de Todos los Santos' }, { date: '2026-12-07', name: 'Día de la Constitución' }],
+    'ME': [{ date: '2026-09-17', name: 'Día de Melilla' },{ date: '2026-04-02', name: 'Jueves Santo' },{ date: '2026-03-20', name: 'Fiesta del Fin del Ayuno - Eid al-Fitr' }, { date: '2026-05-27', name: 'Fiesta del Sacrificio - Aid al-Adha' }, { date: '2026-12-07', name: 'Día de la Constitución' }],
+    'MU': [{ date: '2026-06-09', name: 'Día de la Región de Murcia' }, { date: '2026-03-19', name: 'San José' }, { date: '2026-04-02', name: 'Jueves Santo' }, { date: '2026-12-07', name: 'Día de la Constitución' }],
+    'NC': [{ date: '2026-12-03', name: 'San Francisco Javier' }, { date: '2026-03-19', name: 'San José' }, { date: '2026-04-02', name: 'Jueves Santo' }, { date: '2026-04-06', name: 'Lunes de Pascua' }, { date: '2026-11-02', name: 'Día de Todos los Santos' }],
+    'PV': [{ date: '2026-03-19', name: 'San José' }, { date: '2026-04-02', name: 'Jueves Santo' }, { date: '2026-04-06', name: 'Lunes de Pascua' },{ date: '2026-07-25', name: 'Santiago Apóstol' }],
+    'RI': [{ date: '2026-06-09', name: 'Día de La Rioja' }, { date: '2026-04-02', name: 'Jueves Santo' }, { date: '2026-04-06', name: 'Lunes de Pascua' }, { date: '2026-12-07', name: 'Día de la Constitución' }],
+    'VC': [{ date: '2026-10-09', name: 'Día de la Comunitat Valençiana' }, { date: '2026-03-19', name: 'San José' }, { date: '2026-04-06', name: 'Lunes de Pascua' }, { date: '2026-06-24', name: 'Sant Joan' }]
   }
 };
 
@@ -54,7 +56,7 @@ const POSTAL_TO_REGION = {
   '36': 'GA', '37': 'CL', '38': 'CN', '39': 'CB', '40': 'CL',
   '41': 'AN', '42': 'CL', '43': 'CT', '44': 'AR', '45': 'CM',
   '46': 'VC', '47': 'CL', '48': 'PV', '49': 'CL', '50': 'AR',
-  '51': 'MD', '52': 'MD'
+  '51': 'CE', '52': 'ME'
 };
 
 const VacationOptimizer = () => {
